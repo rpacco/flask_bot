de0636a705a5848b818fef59a9ea90c66d94fb0a
Rafael Pacco <rafael.pacco@gmail.com>
2023-02-24 15:28:05 -0300

    included ratelimits

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import telebot
 import os
+from ratelimit import limits, sleep_and_retry
 
 
 TOKEN = os.getenv("api_key")
@@ -9,10 +10,20 @@ bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 URL = "https://echobot-d3d3.onrender.com/"
 
+
+# Set the rate limit to 1 message per second
+@sleep_and_retry
+@limits(calls=1, period=1)
+def send_message(chat_id, text):
+    bot.send_message(chat_id=chat_id, text=text)
+
 @app.route('/' + TOKEN, methods=['POST'])
 def getMessage():
     update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
-    bot.process_new_updates([update])
+    message = update.message
+    if message is not None:
+        # Echo the message back to the user
+        send_message(message.chat.id, message.text)
     return "!", 200
 
 @app.route("/")
@@ -21,9 +32,5 @@ def webhook():
     bot.set_webhook(url=URL + TOKEN)
     return "!", 200
 
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.reply_to(message, message.text)
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
